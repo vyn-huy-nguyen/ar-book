65642ab77a5afb29a741a6105a85a215615414af
Nguyen Quang Huy <nguyenquanghuy@Nguyens-Mac-mini.local>
2026-01-15 15:26:46 +0700

    update deploy

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // ⭐ QUAN TRỌNG
-  output: 'standalone',
-
   // Disable caching for all routes
   async headers() {
     return [
@@ -19,13 +15,19 @@ const nextConfig = {
             key: 'Cache-Control',
             value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
           },
-          { key: 'Pragma', value: 'no-cache' },
-          { key: 'Expires', value: '0' },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
         ],
       },
     ];
   },
-
+  // Enable external scripts for AR.js and A-Frame
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
